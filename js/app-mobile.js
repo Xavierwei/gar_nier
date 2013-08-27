e94ca61ef38c8148d0a550ff1f18b1f3d1a81ba8
tonysh518 <tony@fuel-it-up.com>
2013-08-28 01:07:43 +0800

    m

--- js/app-mobile.js
+++ js/app-mobile.js
@@ -1,8 +1,3 @@
-/**
- * @desc: application for garnier
- * @date: 
- * @author: hdg1988@gmail.com
- */
 jQuery.easing.easeInOutBackLight = function (x, t, b, c, d , s) {
     if (s == undefined) s = 1.70158;
     if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
@@ -62,9 +57,9 @@ function initTangleColor (){
 var needChange
 $(window)
     .scroll(initTangleColor)
-    .scroll(function(){
-        location.hash="#" + $(this).scrollTop();
-    })
+//    .scroll(function(){
+//        location.hash="#" + $(this).scrollTop();
+//    })
     .resize(initTangleColor);
 setTimeout(function(){
     skrollr.init({
