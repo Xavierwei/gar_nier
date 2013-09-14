628cd6b15b6013e3c8a68ccf4246e38b7dd9437f
mrhack <hdg1988@gmail.com>
2013-09-14 23:14:14 +0800

    fix

--- js/app.js
+++ js/app.js
@@ -118,7 +118,7 @@ var loadComplete = function(){
         // init show-text-box scroll
         $('.girl-desc').bind('mousewheel', function(event, delta, deltaX, deltaY) {
             var $wrap = $(this);
-            $wrap.scrollTop( $wrap .scrollTop() - 20 * deltaY );
+            $wrap.scrollTop( $wrap .scrollTop() - 20 * delta );
             return false;
         });
         
