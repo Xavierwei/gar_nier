3621a21cc17e59f057afff1ce33663fb0f4a3b58
mrhack <hdg1988@gmail.com>
2013-11-10 00:53:54 +0800

    fix

--- psshangfengmian/m/js/main.js
+++ psshangfengmian/m/js/main.js
@@ -23,7 +23,8 @@
                 $loading.show();
                 var reader = new FileReader();
                 reader.onload = function (e) {
-                    $homepage.hide();
+                    // hide other pages
+                    $('.page').hide();
                     $checkpage.show();
                     // change checkpage img
                     var $img = $checkpage.find('.photo_com img');
@@ -48,9 +49,57 @@
                 $checkpage.find('.photo_com img')
                     .attr('src') );
         // TODO .. check photo suitable rize
-        
+
     });
 
+    // init change cover btn
+    $('#change_cover').click(function(){
 
-    // init 
+    });
+
+    // init photo resize event
+    !!(function(){
+        var _totalScale = 1;
+        var _totalRotate = 0;
+        var _lastScale ;
+        var _lastRotate ;
+        var _lastTx = 0 ;
+        var _lastTy = 0 ;
+        var _isTransforming = false;
+        var _$img = $('.img_shadow');
+        $('.photo_mask').hammer({
+            transform_always_block: true
+        }).on("transform", function(event) {
+            _isTransforming = true;
+            var gesture = event.gesture;
+            _lastScale = gesture.scale;
+            _lastRotate = (gesture.rotation || 0);
+            var scale = _totalScale * _lastScale;
+            var rotation = _totalRotate + _lastRotate;
+            // change image transform
+            var transform = 'scale(' + scale + ') rotate(' + rotation + 'deg)';
+            _$img.css({
+                'webkitTransform' : transform ,
+                'transform' : transform
+            });
+        })
+        .on('transformend' , function( event ){
+            setTimeout(function(){
+                _isTransforming = false;
+            } , 100);
+            _totalScale *= _lastScale;
+            _totalRotate += _lastRotate;
+        })
+        .on('drag' , function( event ){
+            if( _isTransforming ) return;
+            _$img.css({
+                marginLeft: _lastTx + event.gesture.deltaX,
+                marginTop: _lastTy + event.gesture.deltaY
+            });
+        })
+        .on('dragend' , function( event ){
+            _lastTx += event.gesture.deltaX,
+            _lastTy += event.gesture.deltaY
+        });
+    })();
 })();
