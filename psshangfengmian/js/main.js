fa6da02fe842e51f7b33735cd89110aeadc20735
mrhack <hdg1988@gmail.com>
2013-11-28 17:56:54 +0800

    fix

--- psshangfengmian/js/main.js
+++ psshangfengmian/js/main.js
@@ -29,7 +29,7 @@ $(function(){
         var tarHeight   = $coverImg.height();
         var tarWidth    = $coverImg.width();
         setTimeout(function(){
-            var forExpr = 40;
+            var forExpr = 100;
             var width   = img.width;
             var height  = img.height;
             if( width / height > tarWidth / tarHeight ){
