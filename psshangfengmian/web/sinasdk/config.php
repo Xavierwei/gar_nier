60d82dc270c051d05a25ef9be8d261e5144dd43c
tonysh518 <tony@fuel-it-up.com>
2013-12-06 18:04:11 +0800

    update

--- psshangfengmian/web/sinasdk/config.php
+++ psshangfengmian/web/sinasdk/config.php
@@ -1,5 +1,5 @@
 <?php
 
-define( "WB_AKEY" , '3240916835' );
-define( "WB_SKEY" , '1a87dd295cdf6d7d74164ef4b30b6bdb' );
+define( "WB_AKEY" , '2953164883' );
+define( "WB_SKEY" , 'c206566dee2770d84e4305bb6eedd3cc' );
 define( "WB_CALLBACK_URL" , 'http://garnier.local:8888/gar_nier/psshangfengmian/web/index.php?r=user/sinacallback' );
